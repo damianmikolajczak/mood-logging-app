98b27641e2fae25001fa093583749a88cd07bd83
damianmikolajczak <damian_mikolajczak@icloud.com>
2021-05-26 11:22:46 +0200

    Initial commit

---
+++ Mood logging app/Mood.swift
@@ -0,0 +1,9 @@
+//
+//  Mood.swift
+//  Mood logging app
+//
+//  Created by Damian Mikołajczak on 26/05/2021.
+//
+
+import Foundation
+
---
+++ Mood logging app/MoodsSetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MoodsSetViewController.swift
+//  Mood logging app
+//
+//  Created by Damian Mikołajczak on 26/05/2021.
+//
+
+import UIKit
+
+class MoodsSetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
