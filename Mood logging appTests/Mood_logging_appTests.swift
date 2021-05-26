3aadffa63980f2fe9206b5e5dd1399fd1a4219b6
damianmikolajczak <damian_mikolajczak@icloud.com>
2021-05-26 10:57:05 +0200

    Initial Commit

---
+++ Mood logging appTests/Mood_logging_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  Mood_logging_appTests.swift
+//  Mood logging appTests
+//
+//  Created by Damian Mikołajczak on 26/05/2021.
+//
+
+import XCTest
+@testable import Mood_logging_app
+
+class Mood_logging_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
